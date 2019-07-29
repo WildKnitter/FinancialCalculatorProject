cc9399da2093eeaee5514bbc899486116c62572a
Pamela Belknap <pbelknap@cox.net>
2019-07-29 16:50:52 -0400

    Fix for CD Calculator

--- js/calculatecd.js
+++ js/calculatecd.js
@@ -3,10 +3,20 @@
 // assuming compound interest.
 //Author:  Pam Belknap
 
+/*This scripts contains code to calculate the future value of an initial investment, 
+* assuming compound interest.
+* @param depAmount (number) - the deposit amount
+* @param intCdRate (number) - the CD's annual interest rate
+* @param loanCdLgth (number) - the length of time in years the CD is held.
+Author:  Pam Belknap
+*/
+
 function init() 
 {
     const btnCalc = document.getElementById("calcCd");
     btnCalc.onclick = calcCd;
+    const btnReset = document.getElementById("resetBtn");
+    btnReset.onclick = resetBtn;
 }
 
 //DETERMINE FUTURE VALUE AND TOTAL EARNED INTEREST
@@ -19,16 +29,32 @@ function calcCd()
     let loanCdLgth = document.getElementById("loanCdLength").value;
     loanCdLgth = Number(loanCdLgth);
 
-    let intCdRateD = (intCdRate/100);
+    let intCdRateD = (intCdRate/1200);
      
-    let futureValueTotal = depAmount * (Math.pow(1 + intCdRateD,loanCdLgth));
+    let futureValueTotal = depAmount * (Math.pow(1 + intCdRateD,(loanCdLgth*12)));
     let intEarnedTotal = futureValueTotal - depAmount;
-
+    
+    if (depAmount < 0) 
+    {
+        const errorMsg = document.getElementById("errorMsg");
+        errorMsg.innerHTML = "One or more of your inputs is a negative number!";
+    }
+    else
+    {
+        const errorMsg = document.getElementById("errorMsg");
+        errorMsg.innerHTML = " ";
+        
     const resultFV = document.getElementById("answerFV");
-    resultFV.value = futureValueTotal.toFixed(2);  //to put a value in an input, use value.
+    resultFV.value = futureValueTotal.toFixed(2);  
 
     const resultIntEarn = document.getElementById("answerIntEarn");
-    resultIntEarn.value = intEarnedTotal.toFixed(2);  //to put a value in an input, use value.
+    resultIntEarn.value = intEarnedTotal.toFixed(2);  
+    }
+}
+
+function resetBtn() 
+{
+    document.getElementById("resetBtn").reset();
 }
 
 window.onload = init;
